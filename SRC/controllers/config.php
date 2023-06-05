e7b9f1e906c6daa044a5742140efe05936a9506b
Nicolas DH <117365736+Pney56@users.noreply.github.com>
2023-06-05 19:47:33 +0200

    1.1
    1.1

--- SRC/controllers/config.php
+++ SRC/controllers/config.php
@@ -1,5 +1,5 @@
 <?php
-define('CLIENT_ID', '42242cd324830a34f2d003916043761f');
-define('CLIENT_SECRET', 'e5a18651bf8a65b12ea3689f57778fe33a5e0d99dad10ddf89352309e702bed9');
-define('REDIRECT_URI', 'http://localhost/test/PROJET-FINAL/?route=apiCallback');
+define('CLIENT_ID', 'VOTRE CLEF API');
+define('CLIENT_SECRET', 'VOTRE CLIENT SECRET ');
+define('REDIRECT_URI', 'REDIRECT DU SITE');
 ?>
